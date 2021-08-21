3a847e99661a08830e90b40bde56073909947b34
Léo Moraes <leohmoraes@users.noreply.github.com>
2021-08-21 10:49:49 -0300

    changes of style guide

---
+++ brisaboa/css.js
@@ -0,0 +1,9 @@
+
+        $(".powered-by").hide();
+        $(" div.js-hide-footer-while-scrolling.footer.clear-both > div > div:nth-child(1) > div:nth-child(1)").hide();
+        $(" div.welcome-message.text-center.p-all-half").hide();
+        $(".desktop-head-top").css("background-color","#3d4069");
+        $(".img-absolute-centered").css("border","6px solid #3d4069");
+
+    loadCSS( '//cdn-alpha.vercel.app/brisaboa/style.css', document.getElementById("style-css"));
+    
